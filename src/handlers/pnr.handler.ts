46b4e61a546a6f7935b4825a976272c8408a5275
cheerifyvibes <cheerifyvibes@gmail.com>
2025-04-22 18:24:04 +0530

    error log

--- src/handlers/pnr.handler.ts
+++ src/handlers/pnr.handler.ts
@@ -10,6 +10,8 @@ export const getPnrStatus = async (req: Request, res: Response) => {
     res.json(pnrData);
     console.log('PNR Data:', pnrData);
   } catch (error) {
+    const { message } = error as Error;
+    console.log('Error fetching PNR status:', message || error);
     res.status(500).json({ error: 'Failed to fetch PNR status' });
   }
 };
